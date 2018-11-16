7f48cb4666ce50e8b0cea873f1d10e930b9e491b
Pepe <Pepe@DESKTOP-K6MVHS3>
2018-11-16 23:35:47 +0100

    JAVA

--- src/Ejercicios/P1_1.java
+++ src/Ejercicios/P1_1.java
@@ -13,9 +13,8 @@ public class P1_1 {
 //c) Implementar una solución recursiva lineal final (C y JAVA).   X/2
 //d) Definir los tamaños y calcular los T(n) para las distintas funciones implementadas considerando los casos mejor y peor.   
 
-	
 //	 A) -------------------------------ITERATIVA----------------------------------------
-	
+
 	public static Integer cuentaMinus(String minusculas) {
 		Integer contMinus = 0;
 		Integer contLetra = 0;
@@ -28,100 +27,89 @@ public class P1_1 {
 			}
 		}
 		return contMinus;
-}
+	}
 
 	public static String Iterativo1(List<String> ls) {
-	    Integer i = 0;
+		Integer i = 0;
 		String palabra = null;
 		Integer comp = 0;
-		while (i < ls.size()-1 ) {
-			if(cuentaMinus(ls.get(i))<cuentaMinus(ls.get(i+1))){
-				if(cuentaMinus(ls.get(i+1))  > comp) {
-					comp = cuentaMinus(ls.get(i+1));
-					palabra = ls.get(i+1);
-				}	
-				
-			}else {
-				if(cuentaMinus(ls.get(i))  > comp) {
+		while (i < ls.size() - 1) {
+			if (cuentaMinus(ls.get(i)) < cuentaMinus(ls.get(i + 1))) {
+				if (cuentaMinus(ls.get(i + 1)) > comp) {
+					comp = cuentaMinus(ls.get(i + 1));
+					palabra = ls.get(i + 1);
+				}
+
+			} else {
+				if (cuentaMinus(ls.get(i)) > comp) {
 					comp = cuentaMinus(ls.get(i));
 					palabra = ls.get(i);
-				}	
+				}
 			}
 			i++;
 		}
 		return palabra;
-		
+
 	}
 //B) -------------------------------------------JAVA10----------------------------------------------------
 
-	
 	public static Integer cuentaCaracteresJ10(String s) {
-		return (int)Stream.iterate(0,x->x+1)
-				.limit(s.length())
-				.filter(x-> Character.isLowerCase(s.charAt(x)))
+		return (int) Stream.iterate(0, x -> x + 1).limit(s.length()).filter(x -> Character.isLowerCase(s.charAt(x)))
 				.count();
-				
+
 	}
-	
-	
-	
+
 	public static String java10(List<String> ls) {
-		return ls.stream()
-				.max(Comparator.comparingInt(x->cuentaCaracteresJ10(x)))
-				.get();
-		
-				
+		return ls.stream().max(Comparator.comparingInt(x -> cuentaCaracteresJ10(x))).get();
+
 	}
+
 //C)-----------------------------------------RECURSIVO---------------------------------------------------
 	public static Integer cuentaMinRec(String n, Integer a, Integer i) {
 		if (n.length() == i) {
 			return a;
-		}else if (!(Character.isUpperCase(n.charAt(i)))) {
+		} else if (!(Character.isUpperCase(n.charAt(i)))) {
 			return cuentaMinRec(n, a + 1, i + 1);
 		} else {
 			return cuentaMinRec(n, a, i + 1);
 		}
 	}
-	
 
 	public static String Ej1RecFinal(List<String> ls, String g, Integer i) {
 		if (ls.size() == 1) {
 			return ls.get(i);
-		}else if(ls.size()-1 == i) {
+		} else if (ls.size() - 1 == i) {
 			return g;
-		}else if (cuentaMinRec(ls.get(i), 0, 0) < cuentaMinRec(ls.get(i + 1), 0, 0)) {
-			if(cuentaMinRec(ls.get(i+1), 0, 0) > cuentaMinRec(g, 0, 0)) {
-				return Ej1RecFinal(ls, ls.get(i+1), i+1);
+		} else if (cuentaMinRec(ls.get(i), 0, 0) < cuentaMinRec(ls.get(i + 1), 0, 0)) {
+			if (cuentaMinRec(ls.get(i + 1), 0, 0) > cuentaMinRec(g, 0, 0)) {
+				return Ej1RecFinal(ls, ls.get(i + 1), i + 1);
 			}
-			return Ej1RecFinal(ls, g, i+1);
-		} else if(cuentaMinRec(ls.get(i), 0, 0) > cuentaMinRec(g, 0, 0)) {
-			
-			return Ej1RecFinal(ls,ls.get(i), i+1);
+			return Ej1RecFinal(ls, g, i + 1);
+		} else if (cuentaMinRec(ls.get(i), 0, 0) > cuentaMinRec(g, 0, 0)) {
+
+			return Ej1RecFinal(ls, ls.get(i), i + 1);
 		}
-	return g;
+		return g;
 	}
-	
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		List<String> e1 = new ArrayList<>();
 		e1.add("aaaaaaa");
 		e1.add("eedf");
 		e1.add("aaaaasvdsvsdvsab");
 		e1.add("aaweFFFFsaahhhhhhjjnbkkkka");
 		e1.add("DDsssDDDD");
-		
-		
-	//	System.out.println(cuentaMinRecursivo("PpepPe", 0, 0));
-		
-		System.out.println(Ej1RecFinal(e1, "", 0));
-		
-	//System.out.println(java10(e1));
 
-	//System.out.println(Iterativo1(e1));
+		System.out.println("Recursivo: "+Ej1RecFinal(e1, "", 0));
+
+		System.out.println("JAVA 10:   "+java10(e1));
+
+		System.out.println("Iterativo: "+Iterativo1(e1));
 
 		// String a = "pepe";
 		// System.out.println(devuelveMinus(a));
+		// System.out.println(cuentaMinRecursivo("PpepPe", 0, 0));
 
 	}
 
--- src/Ejercicios/P1_4.java
+++ src/Ejercicios/P1_4.java
@@ -1,10 +1,9 @@
 package Ejercicios;
 
 import java.util.ArrayList;
-
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 public class P1_4 {
 
@@ -21,6 +20,8 @@ public class P1_4 {
 	// Iterator<String> it= lineas.iterator();
 	// while(it.hasNext()) {
 	// string linea = it.next(); }
+
+//	 A) -------------------------------ITERATIVA----------------------------------------
 
 	public static List<Integer> ordenada(List<Integer> l1, List<Integer> l2) {
 		List<Integer> res = new ArrayList<>();
@@ -52,17 +53,69 @@ public class P1_4 {
 		return res;
 
 	}
+	// B)
+	// -------------------------------------------JAVA10----------------------------------------------------
 
-	public static List<Integer> ordenadaJAVA10(List<Integer> a, List<Integer> b) {
+	public static Integer pos1 = 0, pos2 = 0;
 
-		List<Integer> res = Stream.concat(a.stream(), b.stream()).sorted().collect(Collectors.toList());
-		return res;
+	public static List<Integer> ordenadaJAVA10(List<Integer> l1, List<Integer> l2) {
+
+		return IntStream.range(0, l1.size() + l2.size()).boxed().map(c -> compara(l1, l2)).collect(Collectors.toList());
 
 	}
+
+	private static Integer compara(List<Integer> l1, List<Integer> l2) {
+		Integer res = 0;
+		if (l1.size() == pos1) {
+			res = l2.get(pos2);
+			pos2++;
+		} else if (l2.size() == pos2) {
+			res = l1.get(pos1);
+			pos1++;
+		} else if (l1.get(pos1) < l2.get(pos2)) {
+			res = l1.get(pos1);
+			pos1++;
+		} else if (l2.get(pos2) < l1.get(pos1)) {
+			res = l2.get(pos2);
+			pos2++;
+		} else if (l1.get(pos1) == l2.get(pos2)) {
+			res = l1.get(pos1);
+			pos1++;
+
+		}
+		return res;
+	}
+	// C)-----------------------------------------RECURSIVO---------------------------------------------------
+//Tdo lo que esta encima del while se pone en la cabecera, y se llama al metdo para incrementar
+
+	public static List<Integer> ordenadaRecursivo(List<Integer> l1, List<Integer> l2, List<Integer> res, Integer a,
+			Integer b) {
+
+		Integer d = (l1.size() + l2.size()) - 1;
+		if (res.size() > d) {
+			return res;
+		} else if (a == l1.size() || l1.get(a) > l2.get(b)) {
+			res.add(l2.get(b));
+			return ordenadaRecursivo(l1, l2, res, a, b + 1);
+		} else if (b == l2.size() || l1.get(a) < l2.get(b)) {
+			res.add(l1.get(a));
+			return ordenadaRecursivo(l1, l2, res, a + 1, b);
+
+		} else {
+			res.add(l1.get(a));
+			res.add(l2.get(b));
+			return ordenadaRecursivo(l1, l2, res, a + 1, b + 1);
+
+		}
+	}
+
+	// -------------------------------------------TESTS----------------------------------------------------
 
 	public static void main(String[] args) {
 		List<Integer> a = new ArrayList<>();
 		List<Integer> b = new ArrayList<>();
+		List<Integer> res = new ArrayList<>();
+
 		a.add(1);
 		a.add(4);
 		a.add(6);
@@ -71,8 +124,9 @@ public class P1_4 {
 		b.add(4);
 		b.add(16);
 
-		System.out.println(ordenada(a, b));
-		// System.out.println(ordenadaJAVA10(a, b));
+		System.out.println("Iterativo: " + ordenada(a, b));
+		System.out.println("JAVA 10:   " + ordenadaJAVA10(a, b));
+		System.out.println("Recursivo: " + ordenadaRecursivo(a, b, res, 0, 0));
 	}
 
 }
